fa724a16d6c5523048343401568f61d6166a4fc3
Felicia Bacon <feliciaiveliz@gmail.com>
2022-08-01 12:45:45 -0500

    required data.js file in app.js, added bootstrap, updated react and react-dom to latest

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data'
+import { Table } from 'react-bootstrap'
 
 const App = () => (
-  <div className="app">
+  <div className="app container">
   <header className="header">
     <h1 className="title">Airline Routes</h1>
   </header>
@@ -10,6 +12,19 @@ const App = () => (
     <p>
       Welcome to Airlines App!
     </p>
+  </section>
+  <section>
+    <Table striped>
+      <tbody>
+        {data.routes.map(route => 
+          <tr>
+            <td>{route.airline}</td>
+            <td>{route.src}</td>
+            <td>{route.dest}</td>
+          </tr>
+        )}
+      </tbody>
+    </Table>
   </section>
 </div>
 )
